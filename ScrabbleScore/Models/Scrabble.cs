1ecbccd7196d4f46eaf8ab8c105d0dfeca20776e
Sean Downs <spdowns@protonmail.com>
2020-07-16 11:25:57 -0700

    add a test to check the inputted letter and return the matched value from the dictionary object. FAIL
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>
    Co-Authored-By: Spencer Moody <spencer.moody@outlook.com>

--- ScrabbleScore/Models/Scrabble.cs
+++ ScrabbleScore/Models/Scrabble.cs
@@ -1,8 +1,27 @@
+using System.Collections.Generic;
+
 namespace ScrabbleScore.Models
 {
   public static class Scrabble
   {
-    // properties, constructors, methods, etc. go here
+    private static Dictionary<string, int> _letterScore = new Dictionary<string, int>
+    {
+      { "a", 1 }
+    };
+
+  public static int GetScore(string letter)
+  {
+    if (_letterScore.ContainsKey(letter))
+    {
+      return 0;
+    }
+    else
+    {
+      return 0;
+    }
+
+  } 
     
   }
+
 }
